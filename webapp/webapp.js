4a96e2276a85f7c36bccdef9cd600a0e5a4934da
Alexandra Berke <alexandra_berke@brown.edu>
2012-11-29 20:37:04 -0500

    css changes because I like css

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -13,63 +13,67 @@ $(document).ready(function(){
 	else{
 		connect();
 		function connect(){	
-			var EC2 	= new WebSocket("ws://ec2-174-129-190-18.compute-1.amazonaws.com:8080/websocket");
-			var laptop  = new WebSocket("ws://localhost:6354/websocket");
-	
-			//message('EC2 Status: '+EC2.readyState); 
-			//message('laptop Status: '+laptop.readyState);
-
-			/* inform the user of the actions of the remote websocket */
-			EC2.onopen = function(){
-				message("EC2 websocket opened");
-			};
-			EC2.onclose = function(){
-				message("EC2 websocket closed");
-			};
-
-			/* inform the user of the actions of the local websocket */
-			laptop.onopen = function(){
-				message("laptop websocket opened");
-			};
-			laptop.onclose = function(){
-				message("laptop websocket closed");
-			};
-
-
-			/* Now when we receive a message from either of them,
-			   we simply send it to the other */
-			EC2.onmessage = function(msg){
-				message("Received message '" + msg + "' from the EC2 server. Forwarding to the laptop.");
-				laptop.send(msg);
-			}
-
-			/* symmetrically */
-			laptop.onmessage = function(msg){
-				message("Received message '" + msg + "' from the laptop. Forwarding to the EC2 server.");
-				EC2.send(msg);
-			}
-
-			/* That's it! Now just provide a way of closing them */
-			$('#disconnect').click(function(){
-				EC2.close();
-				laptop.close();
-			});
-
-			$('#send').click(function(){	
-				var toRemote = $('#toRemote').val();
-				var toLocal  = $('#toLaptop').val();
+			try{ //this should be in a try-catch block so we can log errors
+				var EC2 	= new WebSocket("ws://ec2-174-129-190-18.compute-1.amazonaws.com:8080/websocket");
+				var laptop  = new WebSocket("ws://localhost:6354/websocket");
 		
-				if(toRemote){
-					message("Sending to remote: " + toRemote);
-					EC2.send(toRemote);
+				//message('EC2 Status: '+EC2.readyState); 
+				//message('laptop Status: '+laptop.readyState);
+	
+				/* inform the user of the actions of the remote websocket */
+				EC2.onopen = function(){
+					message("EC2 websocket opened");
+				};
+				EC2.onclose = function(){
+					message("EC2 websocket closed");
+				};
+	
+				/* inform the user of the actions of the local websocket */
+				laptop.onopen = function(){
+					message("laptop websocket opened");
+				};
+				laptop.onclose = function(){
+					message("laptop websocket closed");
+				};
+	
+	
+				/* Now when we receive a message from either of them,
+				   we simply send it to the other */
+				EC2.onmessage = function(msg){
+					message("Received message '" + msg + "' from the EC2 server. Forwarding to the laptop.");
+					laptop.send(msg);
 				}
-				
-				if(toLocal){
-					message("Sending to laptop: " + toLocal);
-					laptop.send(toLocal);
+	
+				/* symmetrically */
+				laptop.onmessage = function(msg){
+					message("Received message '" + msg + "' from the laptop. Forwarding to the EC2 server.");
+					EC2.send(msg);
 				}
-			});
-
+	
+				/* That's it! Now just provide a way of closing them */
+				$('#disconnect').click(function(){
+					EC2.close();
+					laptop.close();
+				});
+	
+				$('#send').click(function(){	
+					var toRemote = $('#toRemote').val();
+					var toLocal  = $('#toLaptop').val();
+			
+					if(toRemote){
+						message("Sending to remote: " + toRemote);
+						EC2.send(toRemote);
+					}
+					
+					if(toLocal){
+						message("Sending to laptop: " + toLocal);
+						laptop.send(toLocal);
+					}
+				});
+			}
+			catch(exception){
+				message('<p>Error: '+exception); 			
+			}
 		}
 		
 		/* simply for informing the user of an event */
